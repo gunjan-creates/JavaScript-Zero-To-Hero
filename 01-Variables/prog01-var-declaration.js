5bc72e16c4506043f000af69e0c8b36bc13d5944
Gunjan Ghangare <gunjanghangare590@gmail.com>
2025-11-17 05:45:38 +0000

    .

---
+++ 01-Variables/prog01-var-declaration.js
@@ -0,0 +1,8 @@
+// Demonstrates declaring variables with var, let, and const
+var language = "JavaScript";
+let version = 2025;
+const creator = "Brendan Eich";
+
+console.log("Language:", language);
+console.log("Version:", version);
+console.log("Creator:", creator);

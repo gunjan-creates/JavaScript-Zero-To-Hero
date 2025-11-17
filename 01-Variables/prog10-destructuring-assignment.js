54217e3c65e48144bf579c7771a50041d1b55f96
Gunjan Ghangare <gunjanghangare590@gmail.com>
2025-11-17 05:46:35 +0000

    .#

---
+++ 01-Variables/prog10-destructuring-assignment.js
@@ -0,0 +1,6 @@
+// Uses destructuring to pull values into dedicated variables
+const coordinates = [12.5, -7.3];
+const [latitude, longitude] = coordinates;
+
+console.log("Latitude:", latitude);
+console.log("Longitude:", longitude);

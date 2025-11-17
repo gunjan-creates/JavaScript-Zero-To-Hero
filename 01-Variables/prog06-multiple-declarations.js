5a664aaa32976c7c22f28f2d2b78bc2a385070c6
Gunjan Ghangare <gunjanghangare590@gmail.com>
2025-11-17 05:46:07 +0000

    .#

---
+++ 01-Variables/prog06-multiple-declarations.js
@@ -0,0 +1,6 @@
+// Declares several related variables in one statement
+let firstName = "Alex",
+  lastName = "Morgan",
+  fullName = `${firstName} ${lastName}`;
+
+console.log("Full name:", fullName);
